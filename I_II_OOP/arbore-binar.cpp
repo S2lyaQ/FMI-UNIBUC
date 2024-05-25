acda268c25719c44d2e10f86274f88868a6c4445
Andreea-Diana Chiriac <74276041+kiri-ad@users.noreply.github.com>
2024-05-25 23:34:04 +0300

    operatorul =

--- I_II_OOP/arbore-binar.cpp
+++ I_II_OOP/arbore-binar.cpp
@@ -11,13 +11,43 @@ public:
     Nod(const T& cheie, int contor = 1, Nod* stang = nullptr, Nod* drept = nullptr)
         : cheie(cheie), contor(contor), copilStang(stang), copilDrept(drept) {}
 
+    // Copy constructor
+    Nod(const Nod& alt)
+        : cheie(alt.cheie), contor(alt.contor),
+          copilStang(alt.copilStang ? new Nod(*alt.copilStang) : nullptr),
+          copilDrept(alt.copilDrept ? new Nod(*alt.copilDrept) : nullptr) {}
+
+    // Destructor
+    ~Nod() {
+        delete copilStang;
+        delete copilDrept;
+    }
+
+    // Operatorul de atribuire
+    Nod& operator=(const Nod& alt) {
+        if (this == &alt) return *this;
+
+        cheie = alt.cheie;
+        contor = alt.contor;
+
+        // Eliberăm memoria existentă
+        delete copilStang;
+        delete copilDrept;
+
+        // Copiem noile valori
+        copilStang = alt.copilStang ? new Nod(*alt.copilStang) : nullptr;
+        copilDrept = alt.copilDrept ? new Nod(*alt.copilDrept) : nullptr;
+
+        return *this;
+    }
+
     // Getteri si setteri
     const T& getCheie() const { return cheie; }
     int getContor() const { return contor; }
     Nod* getCopilStang() const { return copilStang; }
     Nod* getCopilDrept() const { return copilDrept; }
 
-    void setCheie(const T& key) { this->cheie = cheie; }
+    void setCheie(const T& cheie) { this->cheie = cheie; }
     void setContor(int contor) { this->contor = contor; }
     void setCopilStang(Nod* copilStang) { this->copilStang = copilStang; }
     void setCopilDrept(Nod* copilDrept) { this->copilDrept = copilDrept; }
@@ -37,6 +67,29 @@ class ArboreBinar {
 public:
     ArboreBinar() : radacina(nullptr) {}
 
+    // Constructor de copiere
+    ArboreBinar(const ArboreBinar& alt) {
+        radacina = alt.radacina ? new Nod<T>(*alt.radacina) : nullptr;
+    }
+
+    // Destructor
+    ~ArboreBinar() {
+        delete radacina;
+    }
+
+    // Operatorul de atribuire
+    ArboreBinar& operator=(const ArboreBinar& alt) {
+        if (this == &alt) return *this;
+
+        // Eliberăm memoria existentă
+        delete radacina;
+
+        // Copiem noua valoare
+        radacina = alt.radacina ? new Nod<T>(*alt.radacina) : nullptr;
+
+        return *this;
+    }
+
     // Getteri si setteri
     Nod<T>* getRadacina() const { return radacina; }
     void setRadacina(Nod<T>* radacina) { this->radacina = radacina; }
@@ -44,5 +97,3 @@ public:
 private:
     Nod<T>* radacina;
 };
-
-
