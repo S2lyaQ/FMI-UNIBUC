689a6825dbdffcf881dde4decc3397d773206baa
Andreea-Diana Chiriac <74276041+kiri-ad@users.noreply.github.com>
2024-06-15 22:53:47 +0300

    Update arbore-binar.cpp

--- I_II_OOP/arbore-binar.cpp
+++ I_II_OOP/arbore-binar.cpp
@@ -9,11 +9,6 @@ using namespace std;
 // 3. chei de tip neprecizat: folosim un tip generic, typename T, iar relatia dintre elementele din arbore este data de relatia dintre cheile lor
 // 4. prevazut cu ordine totala: oricare 2 elemente din arbore pot fi comparate
 
-// Nu avem nevoie de asta pentru nu folosim ArboreBinar in Nod
-// template<typename T>
-// class ArboreBinar;
-
-// Nod
 template<typename T>
 class Nod {
 private:
@@ -26,27 +21,22 @@ public:
     Nod(const T& cheie, int pozitie = 1, Nod* stang = nullptr, Nod* drept = nullptr)
         : cheie(cheie), pozitie(pozitie), copilStang(stang), copilDrept(drept) {}
 
-    // Copy constructor
     Nod(const Nod& alt)
         : cheie(alt.cheie), pozitie(alt.pozitie),
           copilStang(alt.copilStang ? new Nod(*alt.copilStang) : nullptr),
           copilDrept(alt.copilDrept ? new Nod(*alt.copilDrept) : nullptr) {}
 
-    // Destructor
     ~Nod() {
         delete copilStang;
         delete copilDrept;
     }
 
-    // Operatorul de atribuire
     Nod& operator=(const Nod& alt) {
         if (this == &alt) return *this;
 
-        // Eliberăm memoria existentă
         delete copilStang;
         delete copilDrept;
 
-        // Copiem noile valori in inordine
         copilStang = alt.copilStang ? new Nod(*alt.copilStang) : nullptr; 
 
         cheie = alt.cheie;
@@ -57,7 +47,6 @@ public:
         return *this;
     }
 
-    // Operatorul egalitate
     bool operator==(const Nod& alt) const {
         if (cheie != alt.cheie) return false;
         if ((copilStang && !alt.copilStang) || (!copilStang && alt.copilStang)) return false;
@@ -67,27 +56,22 @@ public:
         return true;
     }
 
-    // Operatorul <
     bool operator<(const Nod& alt) const {
         return cheie < alt.cheie;
     }
 
-    // Operatorul >
     bool operator>(const Nod& alt) const {
         return cheie > alt.cheie;
     }
 
-    // Operatorul <=
     bool operator<=(const Nod& alt) const {
         return !(*this > alt);
     }
 
-    // Operatorul >=
     bool operator>=(const Nod& alt) const {
         return !(*this < alt);
     }
 
-    // Operatorul + (Nod + cheieNoua)
     Nod& operator+(const T& cheieNoua) {
         if(cheie != cheieNoua) {
             if(cheie > cheieNoua) {
@@ -107,12 +91,44 @@ public:
         return *this;
     }
 
-    // Getteri si setteri
+    Nod* remove(const T& cheieNoua, Nod* parent = nullptr) {
+        if(cheieNoua < cheie) {
+            if(copilStang) {
+                copilStang = copilStang->remove(cheieNoua, this);
+            }
+        } else if(cheieNoua > cheie) {
+            if(copilDrept) {
+                copilDrept = copilDrept->remove(cheieNoua, this);
+            }
+        } else {
+            if(!copilStang && !copilDrept) {
+                delete this;
+                return nullptr;
+            } else if(!copilStang) {
+                Nod* temp = copilDrept;
+                delete this;
+                return temp;
+            } else if(!copilDrept) {
+                Nod* temp = copilStang;
+                delete this;
+                return temp;
+            } else {
+                Nod* temp = copilDrept;
+                while(temp->copilStang) {
+                    temp = temp->copilStang;
+                }
+                cheie = temp->cheie;
+                copilDrept = copilDrept->remove(temp->cheie, this);
+            }
+        }
+        return this;
+    }
+
     const T& getCheie() const { return cheie; }
     int getPozitie() const { return pozitie; }
     Nod* getCopilStang() const { return copilStang; }
     Nod* getCopilDrept() const { return copilDrept; }
-    Nod* getNodByPosition(int pozitieNod) { // parcurgerea in inordine
+    Nod* getNodByPosition(int pozitieNod) {
         if(pozitie > pozitieNod) {
             if(copilStang) {
                 return copilStang->getNodByPosition(pozitieNod);
@@ -129,7 +145,7 @@ public:
             return nullptr;
         }
 
-        return nullptr; // pentru evitarea erorilor la compilare
+        return nullptr;
     }
 
     void setCheie(const T& cheie) { this->cheie = cheie; }
@@ -138,16 +154,12 @@ public:
     void setCopilDrept(Nod* copilDrept) { this->copilDrept = copilDrept; }
 };
 
-/*****************************************************************************************************/
-
-// ArboreBinar
 template<typename T>
 class ArboreBinar {
 private:
     Nod<T>* radacina;
     int contor;
 
-    // functie pentru actualizarea contorului in inordine (copil stang - radacina - copil drept)
     void updateContor(Nod<T>* radacina) {
         contor = 0;
         updateContorInordine(radacina);
@@ -171,39 +183,32 @@ private:
 public:
     ArboreBinar() : radacina(nullptr), contor(0) {}
 
-    // Constructor de copiere
     ArboreBinar(const ArboreBinar& alt) {
         radacina = alt.radacina ? new Nod<T>(*alt.radacina) : nullptr;
         contor = alt.contor;
     }
 
-    // Destructor
     ~ArboreBinar() {
         delete radacina;
     }
 
-    // Operatorul de atribuire
     ArboreBinar& operator=(ArboreBinar& alt) {
         if (this == &alt) return *this;
 
-        // Eliberăm memoria existentă
         delete radacina;
 
-        // Copiem noua valoare
         radacina = alt.radacina ? new Nod<T>(*alt.radacina) : nullptr;
         contor = alt.contor;
 
         return *this;
     }
 
-    // Operatorul egalitate
     bool operator==(const ArboreBinar& alt) const {
         if (!radacina && !alt.radacina) return true;
         if ((radacina && !alt.radacina) || (!radacina && alt.radacina)) return false;
         return *radacina == *alt.radacina;
     }
 
-    // Operatorul adunare: cheie + arbore
     ArboreBinar& operator+(const T& cheie) {
         if(radacina) {
             *radacina + cheie;
@@ -216,13 +221,12 @@ public:
         return *this;
     }
 
-    // Operatorul adunare: arbore + arbore
     ArboreBinar& operator+(const ArboreBinar& alt) {
         if(alt.radacina) {
             for(int i = 1; i <= alt.contor; ++i) {
                 Nod<T>* nod = alt.radacina->getNodByPosition(i);
                 if(nod) {
-                    this->radacina + nod->getCheie();
+                    *radacina + nod->getCheie();
                 }
             }
         }
@@ -231,57 +235,39 @@ public:
         return *this;
     }
     
-    // Getteri si setteri
+    ArboreBinar& operator-(const T& cheie) {
+        if(radacina) {
+            radacina = radacina->remove(cheie);
+            updateContor(radacina);
+        }
+        return *this;
+    }
+
+    ArboreBinar& operator-(const ArboreBinar& alt) {
+        if(alt.radacina) {
+            for(int i = 1; i <= alt.contor; ++i) {
+                Nod<T>* nod = alt.radacina->getNodByPosition(i);
+                if(nod) {
+                    *this - nod->getCheie();
+                }
+            }
+        }
+
+        updateContor(radacina);
+        return *this;
+    }
+
     Nod<T>* getRadacina() const { return radacina; }
     void setRadacina(Nod<T>* radacina) { this->radacina = radacina; }
 };
 
-// Operatorul adunare: cheie + arbore
 template<typename T>
 ArboreBinar<T> operator+(const T& cheie, ArboreBinar<T>& arb) {
     return arb + cheie;
 }
 
-int main() {
-    ArboreBinar<int> a, b;
-    int cheieA, cheieB;
-    for(int i = 0; i < 20; ++i) {
-        cheieA = 10 + i;
-        cheieB = -10 - i;
-        a = a + cheieA;
-        b = b + cheieB;
-    }
-
-    // Afisare arbore a
-    cout << "Arborele a: ";
-    for(int i = 1; i <= a.getRadacina()->getPozitie(); ++i) {
-        Nod<int>* nod = a.getRadacina()->getNodByPosition(i);
-        if(nod) {
-            cout << nod->getCheie() << " ";
-        }
-    }
-    cout << endl;
-
-    // Afisare arbore b
-    cout << "Arborele b: ";
-    for(int i = 1; i <= b.getRadacina()->getPozitie(); ++i) {
-        Nod<int>* nod = b.getRadacina()->getNodByPosition(i);
-        if(nod) {
-            cout << nod->getCheie() << " ";
-        }
-    }
-    cout << endl;
-
-    // Arborele a + b
-    a = a + b;
-    cout << "Arborele a + b: ";
-    for(int i = 1; i <= a.getRadacina()->getPozitie(); ++i) {
-        Nod<int>* nod = a.getRadacina()->getNodByPosition(i);
-        if(nod) {
-            cout << nod->getCheie() << " ";
-        }
-    }
-    cout << endl;
-
-    return 0;
+template<typename T>
+ArboreBinar<T> operator-(const T& cheie, ArboreBinar<T>& arb) {
+    return arb - cheie;
 }
+
